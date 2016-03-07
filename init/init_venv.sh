29aeb95ae5cc98939889ef50c54560a8f71f4e77
Kun Fang <kun@pocketgems.com>
2016-03-07 00:21:31 -0800

    config virtualenv

--- init/init_venv.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-pip install -r init/requirements.txt
--- init/install_venv.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-DEFAULT_DIR=~/venv
-
-pip install virtualenvwrapper
-export WORKON_HOME=${2:-$DEFAULT_DIR}
-mkdir -p $WORKON_HOME
-source /usr/local/bin/virtualenvwrapper.sh
-mkvirtualenv $1
